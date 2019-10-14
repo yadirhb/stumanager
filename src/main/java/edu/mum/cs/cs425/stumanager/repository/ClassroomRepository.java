ec8feb9cb8a29e4ab5ccfbd33256dbe6e630cd62
Yadir Hernandez <yadirhb@gmail.com>
2019-10-13 21:59:15 -0500

    Added search, edit, add, remove.

--- src/main/java/edu/mum/cs/cs425/stumanager/repository/ClassroomRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.mum.cs.cs425.stumanager.repository;
-
-import edu.mum.cs.cs425.stumanager.model.Classroom;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClassroomRepository extends CrudRepository<Classroom, Long> {
-}
